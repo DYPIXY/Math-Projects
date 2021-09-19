a8d3391838488f62849b01b0854f7857b5e99873
Dypixy <derick.kunz@gmail.com>
2021-09-19 19:04:10 -0300

    u

--- etc/Random Algorithms/MiddleSquareMethod.java
+++ etc/Random Algorithms/MiddleSquareMethod.java
@@ -1,1 +1,21 @@
- 
+import java.util.Scanner;
+
+public class MiddleSquareMethod
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        String seed = keyboard.nextLine();
+        if(seed == "s")
+        {
+            throw new ArithmeticException("erro lol");
+        }
+
+
+        System.out.println(seed);
+    }
+}
+
+
+
+
